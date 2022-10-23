de16ab15ef9764916346a8a3573c8c27760d4877
recoma96 <seokbong60@gmail.com>
2022-10-23 18:41:24 +0900

    strerror

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # 유닉스 프로그래밍 공부하기
 
-
 ## 목차
 * [에러처리하기](./errors)
-    * [perror](./errors/perror.c)
+    * [perror()](./errors/perror.c)
+    * [strerror()](./errors/strerror.c)
---
+++ errors/strerror.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+
+int main(void) {
+    int fd;
+    if(close(fd) == -1)
+    {
+        fprintf(stderr, "Failed!: %s\n", strerror(errno));
+    }
+}
